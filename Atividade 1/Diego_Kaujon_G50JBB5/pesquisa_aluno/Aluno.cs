a99de95f0f7ccd371979cbed4dd9eeb61bef670c
Gileneri <101202246+Gileneri@users.noreply.github.com>
2023-09-06 14:00:20 -0300

    Atividade 1

---
+++ Atividade 1/Diego_Kaujon_G50JBB5/pesquisa_aluno/Aluno.cs
@@ -0,0 +1,17 @@
+﻿// Classe Aluno
+class Aluno
+{
+    public string Nome { get; private set; }
+    public string RA { get; private set; }
+
+    public Aluno(string nome, string ra)
+    {
+        Nome = nome;
+        RA = ra;
+    }
+
+    public bool VerificarRA(string ra)
+    {
+        return RA == ra;
+    }
+}
---
+++ Atividade 1/Diego_Kaujon_G50JBB5/pesquisa_aluno/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Criar um objeto Aluno para representar o aluno cadastrado
+        Aluno alunoCadastrado = new Aluno("Carlinhos", "12345678910");
+
+        // Solicitar entrada do usuário para o RA
+        Console.Write("Digite o RA do aluno: ");
+        string raDigitado = Console.ReadLine();
+
+        // Verificar se o RA corresponde ao aluno cadastrado
+        if (alunoCadastrado.VerificarRA(raDigitado))
+        {
+            Console.Clear();
+            Console.WriteLine("Aluno encontrado: " + alunoCadastrado.Nome);
+        }
+        else
+        {
+            Console.WriteLine("Esse aluno não existe!");
+        }
+
+        // Pausar para ler a mensagem antes de fechar o programa
+        Console.WriteLine("Pressione qualquer tecla para sair...");
+        Console.ReadKey();
+    }
+}
+
+
